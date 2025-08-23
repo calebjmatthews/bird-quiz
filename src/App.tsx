895295da9d797cacaf7f28ed91c47d4ff04756cb
Caleb Matthews <calebjmatthews@gmail.com>
2025-08-23 08:29:58 -0500

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <div className="responsive-container">
+      <h1>ERM (Environmental Record Matching) Bird Quiz!</h1>
+    </div>
+  );
+}
+
+export default App;
